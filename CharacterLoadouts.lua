548ad2c6ce3edca9a43e2e0675d1a66516d7d98d
Timothy Richards <59814951+timothyrichards@users.noreply.github.com>
2020-06-13 12:07:32 -0700

    Added .gitignore Added equipSet and equipLoadout functions

--- CharacterLoadouts.lua
+++ CharacterLoadouts.lua
@@ -1,15 +1,25 @@
-function setEssences(essences)
-   azeriteEssences = C_AzeriteEssence;
-   essenceList = azeriteEssences.GetEssences()
-   
-   for slot,essence in pairs(essences) do 
-      
+function CharacterLoadouts_equipEssences(essences)
+   local essenceList = C_AzeriteEssence.GetEssences()
+   local id;
+
+   for slot,essence in pairs(essences) do
       for k,v in pairs(essenceList) do
          if v.name == essence then
             id = v.ID;
          end
       end;
-      
-      azeriteEssences.ActivateEssence(id, slot)
+
+      C_AzeriteEssence.ActivateEssence(id, slot)
    end;
+end
+
+function CharacterLoadouts_equipSet(equipmentSetName)
+   local setId = C_EquipmentSet.GetEquipmentSetID(equipmentSetName)
+
+   C_EquipmentSet.UseEquipmentSet(setId)
+end
+
+function CharacterLoadouts_equipLoadout(essences, equipmentSetName)
+   CharacterLoadouts_equipEssences(essences)
+   CharacterLoadouts_equipSet(equipmentSetName)
 end
