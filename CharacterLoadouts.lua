51db19e3562440ae8928482bf59aa2ba8fef5a03
Timothy Richards <59814951+timothyrichards@users.noreply.github.com>
2020-06-13 20:14:50 -0700

    Did a bunch of stuff, addon much more cooler

--- CharacterLoadouts.lua
+++ CharacterLoadouts.lua
@@ -1,33 +1,83 @@
-function CharacterLoadouts_equipSet(equipmentSetName)
-   local setId = C_EquipmentSet.GetEquipmentSetID(equipmentSetName)
+CharacterLoadouts = LibStub("AceAddon-3.0"):NewAddon("CharacterLoadouts")
+AceConsole = LibStub("AceConsole-3.0")
+AceGUI = LibStub("AceGUI-3.0")
 
-   C_EquipmentSet.UseEquipmentSet(setId)
+function CharacterLoadouts:getSets()
+   local equipmentSetIDs = C_EquipmentSet.GetEquipmentSetIDs()
+   local equipmentSets = {}
+
+   for k,v in pairs(equipmentSetIDs) do
+      equipmentSets[k] = C_EquipmentSet.GetEquipmentSetInfo(equipmentSetIDs[k])
+   end
+
+   return equipmentSets
 end
 
-function CharacterLoadouts_equipTalents(talents)
+function CharacterLoadouts:equipSet(set)
+   C_EquipmentSet.UseEquipmentSet(set)
+end
+
+function CharacterLoadouts:getTalents()
+   --
+end
+
+function CharacterLoadouts:equipTalents(talents)
    for i, talent in pairs(talents) do
       LearnTalent(talent)
    end
 end
 
-function CharacterLoadouts_equipEssences(essences)
+function CharacterLoadouts:getEssences()
+   return {
+      C_AzeriteEssence.GetMilestoneEssence(115),
+      C_AzeriteEssence.GetMilestoneEssence(116),
+      C_AzeriteEssence.GetMilestoneEssence(117),
+      C_AzeriteEssence.GetMilestoneEssence(119)
+   }
+end
+
+function CharacterLoadouts:equipEssences(essences)
    local heartSlotIds = { 115, 116, 117, 119 }
-   local essenceList = C_AzeriteEssence.GetEssences()
-   local id;
+   local id
 
-   for index,essence in pairs(essences) do
-      for k,v in pairs(essenceList) do
-         if v.name == essence then
-            id = v.ID;
-
-            C_AzeriteEssence.ActivateEssence(id, heartSlotIds[index])
-         end
-      end;
+   for k,v in pairs(essences) do
+      C_AzeriteEssence.ActivateEssence(v, heartSlotIds[k])
    end;
 end
 
-function CharacterLoadouts_equipLoadout(equipmentSetName, talents, essences)
-   CharacterLoadouts_equipSet(equipmentSetName)
-   CharacterLoadouts_equipTalents(talents)
-   CharacterLoadouts_equipEssences(essences)
+function CharacterLoadouts:equipLoadout(loadout)
+   CharacterLoadouts:equipSet(loadout["set"])
+   CharacterLoadouts:equipTalents(loadout["talents"])
+   CharacterLoadouts:equipEssences(loadout["essences"])
+end
+
+function CharacterLoadouts:createSettingsFrame()
+   local frame = AceGUI:Create("Frame")
+   frame:SetTitle("Character Loadouts Settings")
+   frame:SetStatusText("Remember to be in a rested area when switching profiles!")
+
+   local setDropdown = AceGUI:Create("Dropdown")
+   setDropdown:SetLabel("Equipment Set")
+   setDropdown:SetWidth(200)
+   setDropdown:SetList(CharacterLoadouts:getSets())
+   frame:AddChild(setDropdown)
+end
+
+function CharacterLoadouts:openSettings()
+   AceConsole:Print("<Character Loadouts> Opening settings...");
+   CharacterLoadouts:createSettingsFrame()
+end
+
+function CharacterLoadouts:OnInitialize()
+   AceConsole:Print("<Character Loadouts> Character Loadouts addon has loaded!");
+   --AceConsole:RegisterChatCommand("cls", "openSettings")
+   CharacterLoadouts:openSettings()
+end
+
+function CharacterLoadouts:OnEnable()
+   -- Called when the addon is enabled
+end
+
+function CharacterLoadouts:OnDisable()
+   -- Called when the addon is disabled
 end
